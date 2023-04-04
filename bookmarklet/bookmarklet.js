bfccafbc8da77bcfd70241defdb78eb7f52ac867
sabrinahu618 <syh@mit.edu>
2023-04-04 12:29:41 -0400

    working bookmarklet

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,3 +1,16 @@
-javascript: (() => {
-    alert('Hello, Sabrina!');
-  })();
+const password = document.getElementsByClassName('password')[0];
+const showToggle = document.getElementsByClassName('show-toggle')[0];
+showToggle.addEventListener('click', toggleShow);
+
+let show = false;
+
+function toggleShow() {
+  show = !show;
+  toggleType(show)
+}
+
+function toggleType(visible)  {
+  const passwordType = visible ? 'text' : 'password';
+  password.type = passwordType;
+  showToggle.textContent = visible ? "Hide" : "Show";
+}
